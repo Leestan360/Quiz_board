d630507e0dd0c8714150f635c501bb0db84fb8aa
Leestan360 <stanleymayore2@gmail.com>
2022-05-15 17:05:12 +0300

    Add question one and two in script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const quizData = [
+    {
+        question: "What does HTML stand for?",
+        answer: "HyperTransfer Markup Language",
+        answer_1: "HyperText Markup Language",
+        answer_2: "HyperText Markdown Language",
+        answer_3: "HyperText Marked Language",
+        correct: "answer_1",
+    },
+    {
+        question: "What does CSS stand for?",
+        answer: "Central Style Sheets",
+        answer_1: "Cascading Simple Sheets",
+        answer_2: "Cascading Style Sheets",
+        answer_3: "Cascading Styling Sheets",
+        correct: "answer_2",
+    },
+]
